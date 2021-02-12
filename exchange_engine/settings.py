3a14d195623a24645dc1a240103ccf60bb39db62
Mishkina Daria <dasha.mishkina117@gmail.com>
2021-02-12 21:22:33 +0300

    Mishkina: fix database, we use sqllite3 right now

--- exchange_engine/settings.py
+++ exchange_engine/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,10 +79,8 @@ WSGI_APPLICATION = 'exchange_engine.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 

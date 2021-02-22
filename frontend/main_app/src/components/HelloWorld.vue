aaa2b1d445f7ad45c607aa7306231d1194af8386
Иванченко Антон <anton.333.play@gmail.com>
2021-02-22 19:34:04 +0300

    Ivanchenko: [EDIT] 	[EDIT] main_app/src/components/HelloWorld.vue

--- frontend/main_app/src/components/HelloWorld.vue
+++ frontend/main_app/src/components/HelloWorld.vue
@@ -66,27 +66,6 @@
           </a>
         </v-row>
       </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
     </v-row>
   </v-container>
 </template>

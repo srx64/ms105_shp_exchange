69b4323a7a25795753bb2716a0ecce7edf02efed
Иванченко Антон <anton.333.play@gmail.com>
2021-02-27 19:57:48 +0300

    Ivanchenko: [DELETE, CREATE, EDIT] 	[DELETE] frontend/registrarion_page/node_modules due to 	uselessness 	[CREATE] frontend/registration_page/.gitignore to ignore 	node_modules 	frontend/registration_page/README.md to make start instruction 	[EDIT] frontend/registration_page/src/App.vue to fix autofill 	password confirmation

---
+++ frontend/registration_page/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-main>
+      <Register/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Register from './components/Register';
+
+export default {
+  name: 'App',
+
+  components: {
+    Register,
+  },
+
+  data() {
+    return {
+      
+    }
+  }
+};
+</script>
---
+++ frontend/registration_page/src/components/Template.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: '',
+    
+    data() {
+      return {
+        
+      }
+    }
+  }
+</script>

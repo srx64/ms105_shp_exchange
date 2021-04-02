de25bd1031c3c2e463ab4d7a1c4e29d3b112afdd
Иванченко Антон <anton.333.play@gmail.com>
2021-04-03 01:59:55 +0300

    Ivanchenko: [ADD] 	[ADD] new library Vuex to work with local storage

--- exchange_engine_frontend/src/components/auth/Login.vue
+++ exchange_engine_frontend/src/components/auth/Login.vue
@@ -14,6 +14,7 @@
       <v-form
         ref="form"
         lazy-validation
+        v-on:submit.prevent="login"
       >
         <v-text-field
           v-model="email"
@@ -71,10 +72,21 @@
 <script>
   export default {
     name: 'LoginForm',
+
     data: () => ({
       email: '',
-      show: false,
       password: '',
+      show: false,
+      incorrectAuth: false
     }),
+
+    methods: {
+      login () {
+        axios
+          .get('127.0.0.1:8000/api-token', {
+            username: 
+          })
+      }
+    }
   }
 </script>

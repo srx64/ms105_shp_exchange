b217375d3ab9acd3188d5aca3623e4e9a900ccdf
Mishkina Daria <dasha.mishkina117@gmail.com>
2021-04-03 08:01:22 +0300

    Mishkina: the second version of Bot, it can set table price

--- price_bot.py
+++ price_bot.py
@@ -1,7 +1,7 @@
 import os
-import csv
 import time
 import django
+import pandas
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exchange_engine.settings')
 django.setup()
@@ -10,28 +10,29 @@ from main.models import Stocks, Order, User, Quotes
 
 
 def price_bot():
+    files = next(os.walk('data/'))[2]
+    min_file_length = min([len(open(f'data/{file}', 'r').readlines()) for file in files])
     user = User.objects.get(username='ozon')
-    with open('data/OZON2015-2021.csv', newline='') as csvfile:
-        reader = csv.reader(csvfile)
-        AMOUNT = 10000
-        flag = False
-        for line in reader:
-            if flag:
-                row = line[0].split(';')
-                if row[len(row) - 3:] != '-RT':
-                    name = row[0].split('.')[1].split(':')[0]
-                else:
-                    name = row[0][:-3]
-                stock = Stocks.objects.get(name=name)
-                price = row[7]
-                sale_order = Order(user=user, stock=stock, type=True, price=price, amount=AMOUNT, is_closed=True)
-                sale_order.save()
-                purchase_order = Order(user=user, stock=stock, type=False, price=price, amount=AMOUNT, is_closed=True)
-                purchase_order.save()
-                quote = Quotes(stock=stock, price=price)
-                quote.save()
-                time.sleep(30)
-            flag = True
+    AMOUNT = 10000
+    for i in range(min_file_length):
+        for file in files:
+            df = pandas.read_csv(f'data/{file}', nrows=1, skiprows=i, sep=';')
+            # print(df)
+            name = df.iloc[0][0]
+            if name[len(name) - 3:] != '-RM':
+                name = name.split('.')[1].split(':')[0]
+            else:
+                name = name[:-3]
+            stock = Stocks.objects.get(name=name)
+            price = df.iloc[:, [7]][df.iloc[:, [7]].columns[0]][0]
+            sale_order = Order(user=user, stock=stock, type=True, price=price, amount=AMOUNT, is_closed=True)
+            sale_order.save()
+            purchase_order = Order(user=user, stock=stock, type=False, price=price, amount=AMOUNT,
+                                   is_closed=True)
+            purchase_order.save()
+            quote = Quotes(stock=stock, price=price)
+            quote.save()
+        time.sleep(2)
 
 
 if __name__ == "__main__":

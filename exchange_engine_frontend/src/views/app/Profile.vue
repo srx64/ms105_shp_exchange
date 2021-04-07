61e9e29938375d771171c3a7d8dbe154cd528bac
Daniil Snadkin <dsnadkin@gmail.com>
2021-04-07 22:25:39 +0300

    Snadkin: Profile page and editing profile frontend

--- exchange_engine_frontend/src/views/app/Profile.vue
+++ exchange_engine_frontend/src/views/app/Profile.vue
@@ -1,12 +1,62 @@
 <template>
   <v-container>
-    <v-text-field v-model="username" hint="" label="Никнейм"></v-text-field>
-    <v-text-field v-model="first_name" hint="" label="Имя"></v-text-field>
-    <v-text-field v-model="last_name" hint="" label="Фамилия"></v-text-field>
-    <v-text-field v-model="email" hint="" label="Email"></v-text-field>
-    <v-text-field v-model="balance" hint="" label="Balance"></v-text-field>
+    <h1> {{username}} </h1>
 
-    <v-btn color="" class="mr-0" @click="saveData"> Сохранить </v-btn>
+
+    <!-- <v-row align="center" justify="center"> -->
+      <v-btn absolute color="primary" fab
+              small dark outlined>
+              <v-icon>mdi-pencil</v-icon>
+            </v-btn>
+
+      <v-avatar
+            size="200px"
+            color="primary">
+            <span class="white--text headline">
+              {{ first_name[0] }} {{ last_name[0] }}
+            </span>
+      </v-avatar>
+    <!-- </v-row> -->
+    
+      <v-card class="pa-6">
+        <v-form>
+          <v-text-field v-model="username" hint="" label="Никнейм"></v-text-field>
+          <v-text-field v-model="first_name" hint="" label="Имя"></v-text-field>
+          <v-text-field v-model="last_name" hint="" label="Фамилия"></v-text-field>
+          <v-text-field v-model="email" hint="" label="Email"></v-text-field>
+          <v-text-field v-model="balance" hint="" label="Balance"></v-text-field>
+          <v-btn color="" @click="saveData"> Сохранить </v-btn>
+        </v-form>
+      </v-card>
+
+    <v-card class="pa-6">
+      <v-form>
+        <v-file-input @change="onFile"
+            v-model="file"
+            color="primary"
+            counter
+            label="File input"
+            placeholder="Select your files"
+            prepend-icon="mdi-paperclip"
+            outlined
+            :show-size="1000"
+          >
+            <template v-slot:selection="{ index, text }">
+              <v-chip
+                v-if="index < 2"
+                color="primary"
+                dark
+                label
+                small
+              >
+                {{ text }}
+              </v-chip>
+            </template>
+          </v-file-input>
+        <v-btn x-small color="" class="mr-0" > <v-icon small> mdi-pencil </v-icon> Изменить </v-btn>
+      </v-form>
+    </v-card>
+
   </v-container>
 </template>
 
@@ -21,6 +71,7 @@ export default {
     return {
       username: '',
       email: '',
+      // avatar: '',
       first_name: '',
       last_name: '',
       balance: '',
@@ -69,6 +120,9 @@ export default {
               Authorization: `Bearer ${this.$store.state.accessToken}` 
             } 
           })
+      },
+      onFile(event){
+        console.log(event)
       }
     },
 

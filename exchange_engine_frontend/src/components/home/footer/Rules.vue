ae6a1c3dce78aa1243988dd76256093e58016da5
Kuzmichev Artem <artemkuzmi@gmail.com>
2021-04-10 15:39:38 +0300

    Kuzmichev: Added 'About us' on main page

---
+++ exchange_engine_frontend/src/components/home/footer/Rules.vue
@@ -0,0 +1,15 @@
+<template>
+    <div> 
+        <h1> Правила, обязательные для пользования нашей биржей: </h1> <br>
+        <ul>
+            <li> <b> Раз правило </b> </li>
+            <li> <b> Два правило </b> </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'About'
+    }
+</script>

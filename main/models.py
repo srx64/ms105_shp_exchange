9ad1303c6dcc3a3834b58fcfdb7992529e176f63
Mishkina Daria <dasha.mishkina117@gmail.com>
2021-04-16 15:12:15 +0300

    Mishkina: add print about starting and finishing work in price_bot.py

--- main/models.py
+++ main/models.py
@@ -12,14 +12,7 @@ class User(AbstractUser):
 
 class UserSettings(models.Model):
     user_id = models.ForeignKey(to=User, on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to='avatars/')
-
-    def create_avatar(sender, instance, created, **kwargs):
-        if created:
-            user_setting = UserSettings(user_id=instance, avatar='avatars/preset.jpg')
-            user_setting.save()
-
-    post_save.connect(create_avatar, sender=User)
+    avatar = models.ImageField(upload_to='avatars/', max_length=255, null=True, blank=True)
 
 
 class Stocks(models.Model):
--- price_bot.py
+++ price_bot.py
@@ -10,25 +10,29 @@ from main.models import Stocks, Order, User, Quotes
 
 
 def price_bot():
-    files = next(os.walk('data/'))[2]
-    min_file_length = min([len(open(f'data/{file}', 'r').readlines()) for file in files])
-    user = User.objects.get(username='admin')
-    AMOUNT = 10000
-    for i in range(min_file_length):
-        for file in files:
-            df = pandas.read_csv(f'data/{file}', nrows=1, skiprows=i, sep=';')
-            name = df.iloc[0][0]
-            if name[len(name) - 3:] != '-RM':
-                name = name.split('.')[1].split(':')[0]
-            else:
-                name = name[:-3]
-            stock = Stocks.objects.get(name=name)
-            price = df.iloc[:, [7]][df.iloc[:, [7]].columns[0]][0]
-            Order.objects.create(user=user, stock=stock, type=True, price=price, amount=AMOUNT, is_closed=True)
-            Order.objects.create(user=user, stock=stock, type=False, price=price, amount=AMOUNT, is_closed=True)
-            quote = Quotes(stock=stock, price=price)
-            quote.save()
-        time.sleep(30)
+    try:
+        print('Бот начал работу')
+        files = next(os.walk('data/'))[2]
+        min_file_length = min([len(open(f'data/{file}', 'r').readlines()) for file in files])
+        user = User.objects.get(username='admin')
+        AMOUNT = 10000
+        for i in range(min_file_length):
+            for file in files:
+                df = pandas.read_csv(f'data/{file}', nrows=1, skiprows=i, sep=';')
+                name = df.iloc[0][0]
+                if name[len(name) - 3:] != '-RM':
+                    name = name.split('.')[1].split(':')[0]
+                else:
+                    name = name[:-3]
+                stock = Stocks.objects.get(name=name)
+                price = df.iloc[:, [7]][df.iloc[:, [7]].columns[0]][0]
+                Order.objects.create(user=user, stock=stock, type=True, price=price, amount=AMOUNT, is_closed=True)
+                Order.objects.create(user=user, stock=stock, type=False, price=price, amount=AMOUNT, is_closed=True)
+                quote = Quotes(stock=stock, price=price)
+                quote.save()
+            time.sleep(30)
+    except KeyboardInterrupt:
+        print('Бот остановлен пользователем')
 
 
 if __name__ == "__main__":

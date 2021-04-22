1f53f658728c6927deca7915370fa81571c3afe4
Mishkina Daria <dasha.mishkina117@gmail.com>
2021-04-22 23:10:28 +0300

    Mishkina: delete unnecessary tests.py

--- exchange_engine/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase, Client
-
-
-class IndexPageTestCase(TestCase):
-    def test_index_response(self):
-        c=Client()
-        response=c.get(reversed('profile_editing'))
-        self.assertEqual(response.status_code,200)

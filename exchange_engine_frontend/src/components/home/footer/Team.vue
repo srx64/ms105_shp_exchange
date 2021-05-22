ca4f68056b660109cdc4ea8911424e7814c9ee6c
Rechnoi <ANTON.333.play@gmail.com>
2021-05-22 13:33:34 +0300

    Ivanchenko: [EDIT, FIX]
    [EDIT] about team section
    [FIX] little flaws

--- exchange_engine_frontend/src/components/home/footer/Team.vue
+++ exchange_engine_frontend/src/components/home/footer/Team.vue
@@ -1,8 +1,8 @@
 <template>
   <v-row dense>
     <v-col
-      v-for="person in team"
-      :key="person.nick"
+      v-for="(person, index) in team"
+      :key="index"
       cols="6"
     >
       <v-card>
@@ -12,31 +12,74 @@
           gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
           height="200px"
         >
-          <v-card-title v-text="person.name"></v-card-title>
+          <template v-slot:placeholder>
+            <svg style="width:100%;height:100%" viewBox="0 0 24 24">
+              <path fill="grey" d="M12,4A4,4 0 0,1 16,8A4,4 0 0,1 12,12A4,4 0 0,1 8,8A4,4 0 0,1 12,4M12,14C16.42,14 20,15.79 20,18V20H4V18C4,15.79 7.58,14 12,14Z" />
+            </svg>
+          </template>
+          <v-card-title
+            v-text="person.name"
+          />
+          <v-card-subtitle
+            v-text="person.post"
+            class="grey--text text--lighten-2"
+          />
         </v-img>
 
         <v-card-actions>
-
-          
-
           <v-btn
+            :disabled="!person.description && !person.quote"
             icon
             @click="person.show = !person.show"
           >
             <v-icon>{{ person.show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
           </v-btn>
+
+          <v-spacer/>
+
+          <v-btn
+            v-for="(social, name) in person.socials"
+            :href="social.link"
+            target="_blank"
+            :color="social.color"
+            height="36px"
+            fab
+            dark
+            icon
+            small
+            :key="name"
+          >
+            <v-icon
+            >
+              {{ social.icon }}
+            </v-icon>
+          </v-btn>
         </v-card-actions>
 
         <v-expand-transition>
-          <div v-show="person.show">
+          <v-list-item-content 
+            v-show="person.show"
+            class="pb-0"
+          >
             <v-divider></v-divider>
-
-            <v-card-text
-              class="font-italic"
-            >
-              "{{ person.quote }}"
+            <v-card-text v-if="person.description">
+              {{ person.description }}
             </v-card-text>
-          </div>
+            <v-card-text
+              v-if="person.quote"
+              class="pt-0"
+            >
+              <v-card-text
+                v-html="person.quote.split('\n').join('<br>')"
+                class="font-italic text-center py-0"
+              />
+              <v-card-subtitle
+                class="pt-1 pb-0 text-right font-italic"
+              >
+                &copy;{{ person.name }}
+              </v-card-subtitle>
+            </v-card-text>
+          </v-list-item-content>
         </v-expand-transition>
       </v-card>
     </v-col>
@@ -50,42 +93,55 @@ export default {
   data: () => ({
     team: [
       {
-        nick: 'Rechnoi',
+        name: 'Мишкина Дарья',
+        post: 'Тимлид/Бэкенд-разработчик',
+        socials: {},
+        photo: '',
+        description: '',
+        quote: 
+          `"Понимаете, Андрей Александрович, мы писали рандом и случайно написали кризис"`,
+        show: false
+      },
+      {
         name: 'Иванченко Антон',
         post: 'Фронтенд-разработчик',
-        photo: 'https://i.pinimg.com/564x/0f/14/85/0f148590b392d21b4cc7d1d51193dc45.jpg',
-        description: 'Описание',
-        quote: 'Хехе, работает',
-        socials: [
-
-        ],
+        socials: {
+          vk: {
+            link: 'https://vk.com/anton_ivanchenko',
+            icon: 'mdi-vk',
+            color: '#2787F5'
+          },
+          telegram: 
+          {
+            link: 'https://t.me/RechnoiD',
+            icon: 'mdi-telegram',
+            color: '#0088cc'
+          }
+        },
+        photo: '',
+        description: '',
+        quote:
+          `"Андрей Александрович, мы из будущего, не судите строго"`,
         show: false
       },
       {
-        nick: 'vortexof',
-        name: 'Мишкина Дарья',
-        post: 'Тимлид Бэкенд-разработчик',
-        photo: 'https://i.pinimg.com/564x/0f/14/85/0f148590b392d21b4cc7d1d51193dc45.jpg',
-        description: 'Описание',
-        quote: 'Хехе, работает',
-        show: false
-      },
-      {
-        nick: 'Berezin',
         name: 'Березин Николай',
         post: 'Фронтенд-разработчик',
-        photo: 'https://i.pinimg.com/564x/0f/14/85/0f148590b392d21b4cc7d1d51193dc45.jpg',
-        description: 'Описание',
-        quote: 'Фисштех ❤',
+        socials: {},
+        photo: '',
+        description: '',
+        quote: 
+          `"Я тут ещё ни одного колеса не изобретал, всё хорошо" `,
         show: false
       },
       {
-        nick: 'Frolov',
         name: 'Фролов Максим',
         post: 'Бэкенд-разработчик',
-        photo: 'https://i.pinimg.com/564x/0f/14/85/0f148590b392d21b4cc7d1d51193dc45.jpg',
-        description: 'Описание',
-        quote: 'Хехе, работает',
+        socials: {},
+        photo: '',
+        description: '',
+        quote: 
+          `"runservant"`,
         show: false
       }
     ],

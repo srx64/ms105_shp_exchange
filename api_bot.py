ed5dc8170a96a7a6d75a9dfbaa4ef92451ad6ed8
Mishkina Daria <dasha.mishkina117@gmail.com>
2021-06-11 21:55:18 +0300

    Mishkina: fix API bot and url

--- api_bot.py
+++ api_bot.py
@@ -7,24 +7,26 @@ django.setup()
 
 
 def main():
+    host = 'http://127.0.0.1:8000'
     data = {
         'username': 'vasya',
         'password': 'promprog'
     }
-    api_token_url = 'http://127.0.0.1:8000/api-token/'
+    api_token_url = f'{host}/api-token/'
     tokens = requests.post(api_token_url, data=data).json()
     access_token = tokens['access']
-    orders_url = 'http://127.0.0.1:8000/orders/add/'
+    orders_url = f'{host}/orders/add'
     data = {
         'stock': 'OZON',
         'amount': 1,
         'type': False,
         'price': 0
     }
-    response = requests.post(orders_url, headers={'Authorization': f'Bearer {access_token}'}, data=data)
+    headers = {'Authorization': f'Bearer {access_token}'}
+    response = requests.post(orders_url, headers=headers, data=data)
     print(response)
-    url = 'http://127.0.0.1:8000/api/v1/orders/'
-    response = requests.get(url, headers={'Authorization': f'Bearer {access_token}'})
+    url = f'{host}/api/v1/orders/'
+    response = requests.get(url, headers=headers)
     print(response.json())
 
 

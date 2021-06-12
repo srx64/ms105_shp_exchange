bf13018d7c1a8a44fe2deb629ae59564c230f1fe
Mishkina Daria <dasha.mishkina117@gmail.com>
2021-06-12 16:59:56 +0300

    Mishkina: fix API bot, it works now

--- api_bot.py
+++ api_bot.py
@@ -9,21 +9,21 @@ django.setup()
 def main():
     host = 'http://127.0.0.1:8000'
     data = {
-        'username': 'vasya',
-        'password': 'promprog'
+        'username': input('Enter your username: '),
+        'password': input('Enter your password: ')
     }
     api_token_url = f'{host}/api-token/'
     tokens = requests.post(api_token_url, data=data).json()
     access_token = tokens['access']
     orders_url = f'{host}/orders/add'
     data = {
-        'stock': 'OZON',
-        'amount': 1,
-        'type': False,
-        'price': 0
+        'stock': input('Enter name of stock: '),
+        'amount': int(input('Enter amount of stocks: ')),
+        'type': bool(input('Enter "0" if you want to bue stock(s), else enter "1": ')),
+        'price': int(input('Enter price (or "0" if you want to trade at market price): '))
     }
     headers = {'Authorization': f'Bearer {access_token}'}
-    response = requests.post(orders_url, headers=headers, data=data)
+    response = requests.post(orders_url, headers=headers, json=data)
     print(response)
     url = f'{host}/api/v1/orders/'
     response = requests.get(url, headers=headers)

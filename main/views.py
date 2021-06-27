a96530f8d8755dc4478e74a1c91e887dcc53672d
Maxim Frolov <910465@mail.ru>
2021-06-27 15:08:55 +0300

    Frolov: short and long

--- main/views.py
+++ main/views.py
@@ -119,25 +119,22 @@ class AddOrderView(APIView):
             if order.amount != 0:
 
                 if type == 0 and not portfolio.is_debt:
-                    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!111")
                     if user.balance >= order.amount * order.price:
                         portfolio.count += order.amount
                         user.balance -= order.amount * order.price
                         user.save()
                         portfolio.save()
                     else:
-                        #оброботать ошибку не хватки денег
+                        # обработать ошибку не хватки денег
                         return Response({"detail": "incorrect data"}, status=status.HTTP_400_BAD_REQUEST)
 
                 elif type == 1 and portfolio.count >= order.amount and not portfolio.is_debt:
-                    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!333333")
                     portfolio.count -= order.amount
-                    user.balance += order.amount * stock.price # цена на данный момент
+                    user.balance += order.amount * stock.price
                     user.save()
                     portfolio.save()
 
                 elif type == 1 and portfolio.count == 0 and not portfolio.is_debt:
-                    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!22222")
                     if (setting is None or setting.data['is_active']) or not setting.data['is_active']:
                         if order.amount * order.price <= 100000:
                             portfolio.short_balance += order.amount * order.price
@@ -145,14 +142,12 @@ class AddOrderView(APIView):
                             portfolio.count = -order.amount
                             portfolio.save()
                         else:
-                            #обработать ошибку нельзя торговать шорт при захождении загараницу
+                            # обработать ошибку нельзя торговать шорт при захождении заграницу
                             return Response({"detail": "incorrect data"}, status=status.HTTP_400_BAD_REQUEST)
                     else:
                         # торговать в шорт не возможно
-
                         return Response({"detail": "incorrect data"}, status=status.HTTP_400_BAD_REQUEST)
                 elif type == 1 and portfolio.count < order.amount and portfolio.count != 0 and portfolio.count != 0 and not portfolio.is_debt:
-                    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!4444")
                     if (setting is None or setting.data['is_active']) or not setting.data['is_active']:
                         if (order.amount - portfolio.count) * order.price <= 100000:
                             user.balance += portfolio.count * stock.price # цена на данный момент
@@ -162,14 +157,13 @@ class AddOrderView(APIView):
                             user.save()
                             portfolio.save()
                         else:
-                            # обработать ошибку нельзя торговать шорт при захождении загараницу
+                            # обработать ошибку нельзя торговать шорт при захождении заграницу
                             return Response({"detail": "incorrect data"}, status=status.HTTP_400_BAD_REQUEST)
                     else:
                         # торговать в шорт не возможно
                         return Response({"detail": "incorrect data"}, status=status.HTTP_400_BAD_REQUEST)
 
                 elif type == 1 and portfolio.is_debt:
-                    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!999999")
                     if order.amount * order.price <= 100000:
                         portfolio.short_balance += order.amount * order.price
                         portfolio.count -= order.amount
@@ -177,32 +171,28 @@ class AddOrderView(APIView):
                         portfolio.save()
 
                 elif type == 0 and portfolio.is_debt and portfolio.count < -order.amount:
-                    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!8888888888")
-                    user.balance += (100000 - portfolio.short_balance) - order.amount * stock.price  # цена на данный момент
+                    user.balance += (100000 - portfolio.short_balance) - order.amount * stock.price
                     portfolio.count += order.amount
                     user.save()
                     portfolio.save()
 
                 elif type == 0 and portfolio.is_debt and portfolio.count == -order.amount:
-                    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!77777")
-                    user.balance += (100000 - portfolio.short_balance) - order.amount * stock.price  # цена на данный момент
+                    user.balance += (100000 - portfolio.short_balance) - order.amount * stock.price
                     portfolio.count = 0
                     portfolio.is_debt = False
                     user.save()
                     portfolio.save()
 
                 elif type == 0 and portfolio.is_debt and portfolio.count > -order.amount:
-                    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!6666")
                     if order.amount + portfolio.count * order.price < user.balance:
-                        user.balance += (100000 - portfolio.short_balance) - portfolio.count * stock.price  # цена на данный момент
+                        user.balance += (100000 - portfolio.short_balance) - portfolio.count * stock.price
                         portfolio.count = order.amount + portfolio.count
                         portfolio.is_debt = False
                         user.balance -= portfolio.count * order.price
                         user.save()
                         portfolio.save()
                     else:
-
-                        #оброботать ошибку не хватки денег
+                        # обработать ошибку не хватки денег
                         return Response({"detail": "incorrect data"}, status=status.HTTP_400_BAD_REQUEST)
                 self.margin_call(user)
                 self.set_percentage(portfolio)

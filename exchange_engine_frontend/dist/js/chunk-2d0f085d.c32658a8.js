865ab865f5ab66a5f74af04382becfcf0a3c4403
Mishkina Daria <dasha.mishkina117@gmail.com>
2021-06-30 00:17:42 +0300

    Remove dist from git and fix API bot

--- exchange_engine_frontend/dist/js/chunk-2d0f085d.c32658a8.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0f085d"],{"9d73":function(t,n,o){"use strict";o.r(n);var e=function(){var t=this,n=t.$createElement,o=t._self._c||n;return o("h1")},u=[],s={name:"Logout",methods:{logout:function(){var t=this;this.$store.dispatch("userLogout").then((function(){t.$router.push({name:"Login"})}))}},created:function(){this.logout()}},c=s,i=o("2877"),r=Object(i["a"])(c,e,u,!1,null,null,null);n["default"]=r.exports}}]);
-//# sourceMappingURL=chunk-2d0f085d.c32658a8.js.map

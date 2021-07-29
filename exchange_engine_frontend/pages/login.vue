7a207a81408ae13cc3f115bceb25169bffe08405
Arseniy Barinov <BArseniyy@yandex.ru>
2021-07-29 23:05:09 +0300

    Added login and registration forms

---
+++ exchange_engine_frontend/pages/login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Вход</h1>
+    <Form :submit-form="loginUser" />
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    Form: () => import('@/components/form/login.vue')
+  },
+  methods: {
+    loginUser (logininfo) {
+      console.log(logininfo)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ exchange_engine_frontend/pages/register.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Регистрация</h1>
+    <Form :submit-form="registerUser" />
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    Form: () => import('@/components/form/register.vue')
+  },
+  methods: {
+    registerUser (registrationinfo) {
+      console.log(registrationinfo)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
